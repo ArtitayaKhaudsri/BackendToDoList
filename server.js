bf53ff40102af009727e776a2c5f2a2bb1d50fb5
ArtitayaKhaudsri <79126353+ArtitayaKhaudsri@users.noreply.github.com>
2023-04-05 16:50:44 +0700

    global

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const toDoListRouter = require('./router/toDoListRouter');
 
+app.use(cors());
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/api', toDoListRouter);
 
-app.listen(3000, () => {
-    console.log('Start server at port 3000.')
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log("Start server at port " + port)
 })
+
+module.exports = app;
